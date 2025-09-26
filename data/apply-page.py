4fb44387bca1c1215cdba46800ab97a535113eb9
Ahmad Rifai <riff.blend@gmail.com>
2025-09-26 11:04:09 +0700

    fix wrong parsing

--- data/apply-page.py
+++ data/apply-page.py
@@ -16,27 +16,26 @@ sponsors = ''
 with open('contributors.csv', 'r') as f:
 	lines = f.readlines()
 	for line in lines:
-		parts = line.strip().split(', ')
+		parts = line.strip().split(',')
 		if len(parts) >= 3:
 			login, url, pic = parts
-			contributors += f'* [{login}]({url})\n'
+			contributors += f'* [{login.strip()}]({url.strip()})\n'
 
 with open('contributors-wiki.csv', 'r') as f:
 	lines = f.readlines()
 	for line in lines:
-		parts = line.strip().split(', ')
+		parts = line.strip().split(',')
 		if len(parts) >= 3:
 			login, url, pic = parts
-			wiki_contributors += f'* [{login}]({url})\n'
+			wiki_contributors += f'* [{login.strip()}]({url.strip()})\n'
 
 with open('sponsors.csv', 'r') as f:
 	lines = f.readlines()
 	for line in lines:
-		print("line sponsor:", line)
-		parts = line.strip().split(', ')
+		parts = line.strip().split(',')
 		if len(parts) >= 6:
-			login, url, pic, since, price_usd, is_one_time, index_tier = parts
-			sponsors += f'* [{login}]({url})\n'
+			login, name, url, pic, since, price_usd, is_one_time, index_tier = parts
+			sponsors += f'* [{name.strip()}]({url.strip()})\n'
 
 content = content.format(contributors=contributors, wiki_contributors=wiki_contributors, sponsors=sponsors)
 
